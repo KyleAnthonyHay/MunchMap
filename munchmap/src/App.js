42b001220bea2ed0d746f1eeb94b7937f89b4f9c
KyleAnthonyHay <haykyle917@gmail.com>
2024-01-20 13:04:09 -0500

    login UI done

--- munchmap/src/App.js
+++ munchmap/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from './pages/Login'
+import Login from './components/Login'
 import SignUp from './components/SignUp'
 import  LandingPage  from './pages/LandingPage';
 
