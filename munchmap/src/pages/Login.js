2edaf475116d3cfd884c17611d1e7894c2a20998
KyleAnthonyHay <haykyle917@gmail.com>
2024-01-20 12:47:24 -0500

    login added

--- munchmap/src/pages/Login.js
+++ munchmap/src/pages/Login.js
@@ -1,14 +1,35 @@
 import React from 'react';
-
+import './Login.css'; // Make sure to import the CSS file
 
 function Login() {
-    return (
-        <div>
-            <p>
-                Hello World
-            </p>
+  return (
+    <div className="login-container">
+      <div className="login-form">
+        <div className="toggle-buttons">
+          <button className="toggle-user">User</button>
+          <button className="toggle-admin">Admin</button>
         </div>
-    )
+        <div className="input-group">
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <div className="remember-forgot">
+          <label>
+            <input type="checkbox" /> Remember me
+          </label>
+          <a href="#">Forgot Password?</a>
+        </div>
+        <button className="login-button">Log in</button>
+        <div className="social-login">
+          <button className="google-login">Google</button>
+          <button className="signature-login">M. Signature</button>
+        </div>
+        <div className="register-link">
+          Don’t have an account? <a href="#">Register</a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
