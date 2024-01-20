27dd72ddc374b9396b15897d636e6f1d9df5a14b
KyleAnthonyHay <haykyle917@gmail.com>
2024-01-20 13:00:00 -0500

    login UI done

--- munchmap/src/pages/Login.js
+++ munchmap/src/pages/Login.js
@@ -6,7 +6,7 @@ function Login() {
     <div className="login-container">
       <div className="login-form">
         <div className="toggle-buttons">
-          <button className="toggle-user">Login</button>
+          <button className="toggle-user">User</button>
         </div>
         <div className="input-group">
           <input type="email" placeholder="Email" />
