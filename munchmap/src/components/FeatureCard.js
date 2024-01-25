294af1a12c3d7dfbb66e75b992970659614f5bc7
KyleAnthonyHay <haykyle917@gmail.com>
2024-01-25 10:11:50 -0500

    landing cards

--- munchmap/src/components/FeatureCard.js
+++ munchmap/src/components/FeatureCard.js
@@ -1,17 +1,24 @@
 import { Card, CardContent, Container, Grid, Typography } from '@mui/material'
 import React from 'react'
+import './FeatureCard.css'
+
+import TicketImage from '../assets/Tickets.svg'
+import ServerImage from '../assets/Server.svg'
+import UploadImage from '../assets/Upload.svg'
+// Define your styles object at the top of the file
 
 const FeatureCard = () => {
   return (
-            <Container sx={{ py: 8 }} maxWidth="md">
+            <Container sx={{ py: 8 }} maxWidth="lg">
                 <Grid container spacing={4}>
                     {/* ******************** Card 1 ******************** */}
                     <Grid item xs={12} sm={6} md={4}>
                         <Card>
-                            <CardContent>
+                            <CardContent className="card-content">
                                 <Typography gutterBottom variant="h5" component="h2">
                                     Upload Tickets
                                 </Typography>
+                                <img src={TicketImage} className="card-img" alt="Upload Tickets"/>
                                 <Typography>
                                     This is a brief description of the first feature your app offers.
                                 </Typography>
@@ -21,10 +28,11 @@ const FeatureCard = () => {
                     {/* ******************** Card 2 ******************** */}
                     <Grid item xs={12} sm={6} md={4}>
                         <Card>
-                            <CardContent>
+                            <CardContent className="card-content">
                                 <Typography gutterBottom variant="h5" component="h2">
                                     Upload Tickets
                                 </Typography>
+                                <img src={ServerImage} className="card-img" alt="Upload Tickets"/>
                                 <Typography>
                                     This is a brief description of the first feature your app offers.
                                 </Typography>
@@ -34,12 +42,13 @@ const FeatureCard = () => {
                     {/* ******************** Card 3 ******************** */}
                     <Grid item xs={12} sm={6} md={4}>
                         <Card>
-                            <CardContent>
+                            <CardContent className="card-content">
                                 <Typography gutterBottom variant="h5" component="h2">
-                                    Admin Console
+                                    Upload Tickets
                                 </Typography>
+                                <img src={UploadImage} className="card-img" alt="Upload Tickets"/>
                                 <Typography>
-                                    Connect restaurant tickets to wearhouses that need food.
+                                    This is a brief description of the first feature your app offers.
                                 </Typography>
                             </CardContent>
                         </Card>
