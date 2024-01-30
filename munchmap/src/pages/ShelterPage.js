7bbbc61ef59b3dd537e7f74bc476af924f279e0a
Kevin Abraham <myservices@kabes.org>
2024-01-29 23:08:06 -0500

    adjusted new axios requests

--- munchmap/src/pages/ShelterPage.js
+++ munchmap/src/pages/ShelterPage.js
@@ -1,10 +1,61 @@
 import React from 'react'
+import axios from 'axios'
+import { useState } from 'react';
+import { useEffect } from 'react';
 import SignOutButton from '../components/SignOutButton'
 // MaterialUI Imports
 import Typography from '@mui/material/Typography';
 import { Box, Grid } from '@mui/material';
+import Button from '@mui/material/Button';
 
 const ShelterPage = () => {
+  const [shelter, setShelter] = useState(null);
+  const [quantity_requested, setQuantity] = useState(10);
+  const [food_category, setFoodCategory] = useState(0);
+
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    const fetchShelter = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/api/shelters/get_my_shelter/', {
+          headers: {
+            Authorization: `Token ${token}`
+          }
+        });
+        console.log('Shelter:', response.data);
+        setShelter(response.data);
+      } catch (error) {
+        console.error('Error fetching shelter', error);
+      }
+    };
+    fetchShelter();
+  }
+  , []);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const token = localStorage.getItem('token');
+    const newRequest = {
+      shelter: shelter.id, 
+      quantity_requested: quantity_requested,
+      food_category: food_category
+    };
+
+    console.log(newRequest)
+    try {
+      const response = await axios.post('http://localhost:8000/api/shelter-requests/', newRequest, {
+        headers: {
+          Authorization: `Token ${token}`
+        }
+      });
+      console.log('Request created:', response.data);
+    } catch (error) {
+      console.error('Error creating ticket:', error);
+    }
+  }
+  
+  
   return (
     <div>
       <SignOutButton/>
@@ -20,6 +71,27 @@ const ShelterPage = () => {
         </Box>
       </Grid>
     </Grid>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Quantity:
+        <input
+          type="number"
+          value={quantity_requested}
+          onChange={(event) => setQuantity(event.target.value)}
+        />
+      </label>
+      <label>
+        Food Category:
+        <input
+          type="number"
+          value={food_category}
+          onChange={(event) => setFoodCategory(event.target.value)}
+        />
+      </label>
+      <Button type="submit" variant="contained" color="primary">
+        Create Ticket
+      </Button>
+    </form>
     </div>
   )
 }
